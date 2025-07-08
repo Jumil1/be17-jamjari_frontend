7973019cf5d536280f4b9d1089a7baccca4ea7e4
Jumil1 <js020131@gmail.com>
2025-07-08 17:29:12 +0900

    Add files via upload

---
+++ phanes_editor/src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <RouterLink to="/">Main</RouterLink>
+        <span> </span>
+        <RouterLink to="/create">Create Project</RouterLink>
+        <RouterLink to="/editor">Editor</RouterLink>
+        <RouterLink to="/signup">signUp</RouterLink>
+        <RouterLink to="/create/project">프로젝트 생성</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+</style>
